d98e819d19351500c7e14dbf2444ba3b1b20758d
jenishbekor <57383078+jenishbekor@users.noreply.github.com>
2026-08-04 16:02:44 +0600

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,39 @@
+function Hero() {
+    return (
+        <section id="home" className="hero-section">
+            <div className="container">
+                <div className="row align-items-center min-vh-75">
+
+                    <div className="col-lg-6">
+                        <h1 className="display-4 fw-bold">
+                            Сиздин өзгөчө күнүңүз
+                        </h1>
+
+                        <p className="lead mt-3">
+                            Бүтүрүү аземи үчүн сапаттуу жана стилдүү
+                            мантияларды ижарага алыңыз.
+                        </p>
+
+                        <a
+                            href="#robes"
+                            className="btn btn-dark btn-lg mt-3"
+                        >
+                            Мантияларды көрүү
+                        </a>
+                    </div>
+
+                    <div className="col-lg-6 mt-4 mt-lg-0">
+                        <img
+                            src="src/assets/mantiya.png"
+                            alt="Graduation"
+                            className="img-fluid rounded-4 shadow"
+                        />
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-white shadow-sm">
+            <div className="container">
+                <a className="navbar-brand fw-bold" href="#">
+                    Мантия на прокат
+                </a>
+
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarMenu"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarMenu">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#home">
+                                Башкы бет
+                            </a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#robes">
+                                Мантиялар
+                            </a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#how">
+                                Кантип иштейт?
+                            </a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#contact">
+                                Байланыш
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
