cc7e1aba6e1a54dbac8119c76a190cb698887a4c
user <nuradil.amatzhanov@gmail.com>
2026-09-01 17:07:38 +0600

    robes

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -3,127 +3,127 @@ import RobeCard from './RobeCard'
 function Catalog() {
     const robes = [
         {
-            image: '/blue_with_yellow.png',
+            image: 'blue_with_yellow.png',
             title: 'Синяя с желтой полоской',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/dark_blue_with_s_yellow.png',
+            image: 'dark_blue_with_s_yellow.png',
             title: 'Тёмно синяя с жёлтым тонкая полоска',
             description: 'Элегантная черная мантия с золотой отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/dark_blue_with_b_yellow.png',
+            image: 'dark_blue_with_b_yellow.png',
             title: 'Тёмно синяя с жёлтым широкая полоска',
             description: 'Стильная мантия с яркой красной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/dark_blue_with_yellow.png',
+            image: 'dark_blue_with_yellow.png',
             title: 'Темно синяя c желтым',
             description: 'Классическая мантия с синей отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/mantiya_blue_white.png',
+            image: 'mantiya_blue_white.png',
             title: 'Темно синий с белым',
             description: 'Классическая черная мантия с зеленой отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/blue_elegant_with_white.png',
+            image: 'blue_elegant_with_white.png',
             title: 'Синий элегант с белым',
             description: 'Минималистичная черная мантия для выпускного.',
             price: '500 сом / день'
         },
         {
-            image: '/purple.png',
+            image: 'purple.png',
             title: 'Фиолетовая с желтым',
             description: 'Минималистичная черная мантия для выпускного.',
             price: '500 сом / день'
         },
         {
-            image: '/burgundy_with_yellow.png',
+            image: 'burgundy_with_yellow.png',
             title: 'Бардовая с желтым',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/burgundy_with_white.png',
+            image: 'burgundy_with_white.png',
             title: 'Бардовая с белым',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/green.png',
+            image: 'green.png',
             title: 'Изумрудная с золотым',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/black_with_gold.png',
+            image: 'black_with_gold.png',
             title: 'Черная с золотым',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/black_with_brown.png',
+            image: 'black_with_brown.png',
             title: 'Черная с бордовым',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/mantiya10.png',
+            image: 'mantiya10.png',
             title: 'Черная с розовым',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/mantiya11.png',
+            image: 'mantiya11.png',
             title: 'Черная с красным',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/mantiya12.png',
+            image: 'mantiya12.png',
             title: 'Черная с белым',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/mantiya13.png',
+            image: 'mantiya13.png',
             title: 'Черная с желтым',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/mantiya14.png',
+            image: 'mantiya14.png',
             title: 'Черная с фиолетовым',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/mantiya15.png',
+            image: 'mantiya15.png',
             title: 'Черная с зеленым',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/mantiya16.png',
+            image: 'mantiya16.png',
             title: 'Фиолетовая с желтым',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/mantiya17.png',
+            image: 'mantiya17.png',
             title: 'Бирюзовая с белым',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
         },
         {
-            image: '/mantiya18.png',
+            image: 'mantiya18.png',
             title: 'Синяя с белым',
             description: 'Премиальная модель с аккуратной декоративной отделкой.',
             price: '500 сом / день'
